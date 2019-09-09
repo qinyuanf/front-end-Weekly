f5569a65a34796c9cf9f351d1468f15910e0326c
秦元丰 <790880610@qq.com>
2019-09-09 22:45:47 +0800

    feat: 防抖和节流

---
+++ demo/debounce-and-throttle/index.js
@@ -0,0 +1,38 @@
+// 防抖
+const debounce = (fn, delay) => {
+  // 设置一个 timer
+  let timer = null
+  
+  return function () {
+    // 获取当前 return 函数作用域和参数
+    const args = arguments
+    const vm = this
+    // 清除正在执行的函数
+    clearTimeout(timer)
+    timer = setTimeout(function () {
+      fn.apply(vm, args)
+    }, delay)
+  }
+}
+
+// 节流，时间戳 + 定时器
+const throttle = (fn, delay) => {
+  let timer = null
+  // 开始时间
+  let startTime = Date.now()
+
+  return function () {
+    const endTime = Date.now()
+    // 剩余时间
+    const remainTime = delay - (endTime - startTime)
+    const vm = this
+    const args = arguments
+    clearTimeout(timer)
+    if (remainTime <= 0) {
+      fn.apply(vm, args)
+      startTime = Date.now()
+    } else {
+      timer = setTimeout(fn, remainTime)
+    }
+  }
+}
